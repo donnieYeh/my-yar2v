5ab92eb9fcebe3f6551c1d8e2e3b4a573bb83424
= <=>
2022-01-02 12:35:14 +0800

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,6 +33,7 @@ EOF
 # Get yar2v executable release
 mv /opt/yar2v.zip ${DIR_TMP}/yar2v.zip
 busybox unzip ${DIR_TMP}/yar2v.zip -d ${DIR_TMP}
+ls ${DIR_TMP}
 chmod 755 ${DIR_TMP}/yar2v ${DIR_TMP}/ltc2v
 
 # Convert to protobuf format configuration
